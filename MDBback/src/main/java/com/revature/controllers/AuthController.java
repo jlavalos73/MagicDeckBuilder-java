df107b7c97e700d4b7314a7530439f0bbdcf972f
Jonathan Conner <jonathan.g.conner@gmail.com>
2020-04-14 14:51:50 -0400

    fixed the   login and user

--- MDBback/src/main/java/com/revature/controllers/AuthController.java
+++ MDBback/src/main/java/com/revature/controllers/AuthController.java
@@ -1,5 +1,7 @@
 package com.revature.controllers;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,32 +17,33 @@ import com.revature.models.templates.LoginTemplate;
 import com.revature.repositories.UserDAO;
 import com.revature.services.SecurityService;
 
-
 @RestController
-@RequestMapping(value="/auth")
+@RequestMapping(value = "/auth")
 @CrossOrigin
 public class AuthController {
 
 	@Autowired
 	private UserDAO udao;
-	
+
 	@Autowired
 	private SecurityService sec;
-	
-	@PostMapping
-	public ResponseEntity<User> login(@RequestBody LoginTemplate lt){
-		
-		User user = udao.findByEmail(lt.getEmail());
-		System.out.println(user);
-		
-		boolean valid = sec.checkPassword(lt.getPassword(), user.getPassword());
 
-		if(valid) {
-			return ResponseEntity.status(HttpStatus.OK).body(user);
-		}else {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build(); //Response if user is not found
+	@PostMapping
+	public ResponseEntity<User> login(@RequestBody LoginTemplate lt) {
+
+		Optional<User> optUser = udao.findByEmail(lt.getEmail());
+
+		if (optUser.isPresent()) // proceed when user found
+		{
+			User user = optUser.get();
+			System.out.println(user);
+			boolean valid = sec.checkPassword(lt.getPassword(), user.getPassword());
+			if (valid) {
+				return ResponseEntity.status(HttpStatus.OK).body(user);
+			}
 		}
+		return ResponseEntity.badRequest().build();// Response if user is not found
+
 	}
-	
-	
+
 }
